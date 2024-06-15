34638606b8022b35781dfe8809a864f552766cb6
rohitsawariya <rohitsawariya0@gmail.com>
2024-06-15 14:05:22 +0530

    Make Changes in DateFormat

--- src/Components/Items.jsx
+++ src/Components/Items.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import styles from './Items.module.css';
 
 function Items({ todoName, todoDate, onDelete }) {
+  const formatDate = (dateString) => {
+    const date = new Date(dateString).setHours(0, 0, 0, 0);
+    const today = new Date().setHours(0, 0, 0, 0);
+    const tomorrow = new Date(today).setDate(new Date().getDate() + 1);
+
+    if (date === today) {
+        return "Today";
+    } else if (date === tomorrow) {
+        return "Tomorrow";
+    } else {
+        return new Date(dateString).toLocaleDateString('en-GB', { day: 'numeric', month: 'long', year: 'numeric' });
+    }
+};
+
+
     return (
         <div className={`${styles.set} flex flex-wrap p-2`}>
   <div className="w-full sm:w-1/4 m-2 mx-9 text-white px-5 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600">
@@ -9,7 +24,7 @@ function Items({ todoName, todoDate, onDelete }) {
   </div>
 
   <div className="w-full sm:w-1/4 m-2 mx-8 px-4 py-2 text-white border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600">
-    {todoDate}
+    {formatDate(todoDate)}
   </div>
 
   <button
